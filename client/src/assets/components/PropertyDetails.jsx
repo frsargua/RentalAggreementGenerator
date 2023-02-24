e5f991e3e39440c0b079642cb5d6d920eefcc5d0
frsargua <frsargua@gmail.com>
2023-02-24 17:55:47 +0000

    created function to update property description

--- client/src/assets/components/PropertyDetails.jsx
+++ client/src/assets/components/PropertyDetails.jsx
@@ -1,7 +1,9 @@
 import { TextField, Toolbar, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
-export default function PropertyDetails() {
+export default function PropertyDetails(props) {
+  let { updatePropertyDetails, propertyDetails } = props;
+
   return (
     <>
       <Typography variant="h6" fontWeight={600}>
@@ -11,14 +13,20 @@ export default function PropertyDetails() {
       <TextField
         margin="dense"
         sx={{ mx: 4 }}
+        name="address"
+        value={propertyDetails.address}
         fullWidth
         placeholder="Property Address"
+        onChange={updatePropertyDetails}
       />
       <TextField
         margin="dense"
         sx={{ mx: 4 }}
+        name="description"
+        value={propertyDetails.description}
         fullWidth
         placeholder="Property Description of Rooms"
+        onChange={updatePropertyDetails}
       />
     </>
   );
--- client/src/assets/pages/mainPage.jsx
+++ client/src/assets/pages/mainPage.jsx
@@ -2,7 +2,7 @@ import RentalConditions from "../components/RentalConditions";
 import TenantDetails from "../components/TenantDetails";
 import LandLordDetails from "../components/LandLordDetails";
 import PropertyDetails from "../components/PropertyDetails";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Box } from "@mui/system";
 import { IconButton, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
@@ -10,6 +10,47 @@ import RemoveIcon from "@mui/icons-material/Remove";
 function MainPage() {
   const [numberOfLandLords, setNumberOfLandlords] = useState(0);
   const [numberOfTenants, setNumberOfTenants] = useState(0);
+
+  const [propertyDetails, setPropertyDetails] = useState({
+    address: "",
+    description: "",
+  });
+
+  const [rentalConditions, setRentalConditions] = useState({
+    startingDate: "",
+    duration: "",
+    amount: "",
+    deposit: "",
+  });
+
+  const [landlordsDetails, setLandlordsDetails] = useState({
+    1: { title: "", fullName: "", documentType: "", documentNumber: "" },
+    2: { title: "", fullName: "", documentType: "", documentNumber: "" },
+  });
+  const [tenantsDetails, setTenantsDetails] = useState({
+    1: {
+      title: "",
+      firstNames: "",
+      Surnames: "",
+      documentType: "",
+      documentNumber: "",
+    },
+    2: {
+      title: "",
+      firstNames: "",
+      Surnames: "",
+      documentType: "",
+      documentNumber: "",
+    },
+  });
+
+  function updatePropertyDetails(event) {
+    const { name, value } = event.target;
+
+    setPropertyDetails((prev) => {
+      return { ...prev, [name]: value };
+    });
+  }
 
   function increaseNumberOfTenants() {
     if (numberOfTenants < 2) {
@@ -33,20 +74,26 @@ function MainPage() {
   }
 
   const landlordComponents = [];
+  const tenantsComponents = [];
 
   for (let i = 0; i < numberOfLandLords; i++) {
     landlordComponents.push(<LandLordDetails />);
   }
 
-  const tenantsComponents = [];
-
   for (let i = 0; i < numberOfTenants; i++) {
     tenantsComponents.push(<TenantDetails />);
   }
 
+  useEffect(() => {
+    console.log(propertyDetails);
+  }, [propertyDetails]);
+
   return (
     <>
-      <PropertyDetails />
+      <PropertyDetails
+        updatePropertyDetails={updatePropertyDetails}
+        propertyDetails={propertyDetails}
+      />
       <Box>
         <Typography variant="h6" fontWeight={600}>
           Property Owner/s Details
